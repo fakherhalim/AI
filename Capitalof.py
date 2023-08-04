56afdedbf9b0a57612a94edcac04a6fd9a8e8712
Fakher Halim <fhalim@msn.com>
2023-08-04 17:37:37 -0500

    stored key in Windows Environment: OPENAI_API_KEY

--- Capitalof.py
+++ Capitalof.py
@@ -1,9 +1,8 @@
-
+# first run !pip install openai on the command prompt
 import os
 import openai
 openai.organization = "org-YLFi9xzpghD9In8J4kGESGt0"
-openai.api_key = "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"#"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"
-
+openai.api_key = os.getenv('OPENAI_API_KEY')#"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"
 
 def query_gpt3(prompt):
     try:
@@ -12,7 +11,7 @@ def query_gpt3(prompt):
             engine="text-davinci-003",  # You can choose a different engine depending on your requirem>            ,
             max_tokens=128,  # Set the maximum number of tokens in the response
             stop=None,       # You can specify a stop sequence to end the response early if needed
-            temperature=0.7  # Controls the randomness of the response (lower values make it more focu>       
+            temperature=0.7  #  A higher temperature (e.g., 0.7) results in more diverse and creative output, while a lower temperature (e.g., 0.2) makes the output more deterministic and focused       
         )
 
         return response['choices'][0]['text'].strip()
@@ -29,16 +28,3 @@ response_text = query_gpt3(prompt_text)
 
 # Print the response
 print(response_text)
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
